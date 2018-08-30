8bf4a9e9e7fd428fe5474153166ffa27bc2b0824
mjr390 <mjr117@yahoo.com>
2018-08-29 22:12:06 -0400

    added code to return metadata onto a webpage

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,13 +3,30 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+  var meta = `/metadata/${sample}`
+  d3.json(meta).then(function(response){
+    console.log(response)
 
+    var data = [response]
+    console.log(data)
+  
+    // Use d3 to select the panel with id of `#sample-metadata`
+      var table = d3.select("#sample-metadata").html("")
     // Use `.html("") to clear any existing metadata
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+
+      var cell = table.append("td");
+        Object.entries(response).forEach(([key, value]) => {
+          console.log(key, value)
+          
+          var row = cell.append("tr");
+          row.text(`${key}: ${value}`);
+      });
+    });
+
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
@@ -50,6 +67,7 @@ function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
+  console.log("optionChanged Worked!");
 }
 
 // Initialize the dashboard
